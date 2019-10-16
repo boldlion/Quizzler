ac9951ae5e5684ddf1f2092d8c60e24cf92a51fd
Bold Lion <boldlion1988@gmail.com>
2019-10-16 15:54:06 +0300

    ProgressHUD issue fixed. Small bug fixed Retain cycle fixed

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressBarWidthConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,14 +37,15 @@ class ViewController: UIViewController {
             pickedAnswer = false
         }
         checkAnswer()
-        questionNumber += 1
+        questionNumber = questionNumber + 1
         nextQuestion()
     }
     
     func updateUI() {
         scoreLabel.text = "Score: \(score)"
         progressLabel.text = "\(questionNumber + 1) / 13"
-        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
+        progressBarWidthConstraint.constant = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
+        progressBar.backgroundColor = .red
     }
     
     func nextQuestion() {
@@ -53,7 +55,7 @@ class ViewController: UIViewController {
         }
         else {
             let alert = UIAlertController(title: "End Of Quiz", message: "Do you want to start over?", preferredStyle: .alert)
-            let okayAction = UIAlertAction(title: "Okay", style: .default, handler: { _ in
+            let okayAction = UIAlertAction(title: "Okay", style: .default, handler: { [unowned self] _ in
                 self.startOver()
             })
             alert.addAction(okayAction)
